e9ee7db17ac5e6dd694ea274467ea1c4c7528b7b
Sam Kravitz <samkravitz99@gmail.com>
2022-05-09 20:44:42 -0400

    Add pest crate and implement this grammar: cmd [arg]*

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
 use std::io::{stdin, stdout, Write};
 
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "rush.pest"]
+pub struct PestParser;
+
 fn main() {
 	loop {
 		prompt();
-		let mut input = String::new();
-		stdin().read_line(&mut input).unwrap();
+		let mut line = String::new();
+		stdin().read_line(&mut line).unwrap();
+
+		let parsed = PestParser::parse(Rule::main, line.trim())
+			.expect("Unsuccessful parse")
+			.next()
+			.unwrap();
+
+		let mut cmd: String = String::new();
+		let mut args: Vec<String> = Vec::new();
+
+		for tok in parsed.into_inner() {
+			match tok.as_rule() {
+				Rule::cmd => cmd = String::from(tok.as_str()),
+				Rule::arg => args.push(String::from(tok.as_str())),
+				_ => {}
+			}
+		}
+
+		println!("{} {:?}", cmd, args);
 	}
 }
 
