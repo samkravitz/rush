f1e412a2d838dacdbb364ec88e73d5ca7cc3e52b
Sam Kravitz <samkravitz99@gmail.com>
2022-06-06 09:52:42 -0400

    Parse pipe_list and io_modifier_list

--- src/parse.rs
+++ src/parse.rs
@@ -22,6 +22,55 @@ pub fn parse(input: &str) -> command::Command {
             Rule::cmd_and_args => {
                 command.add_simple_command(String::from(rule.as_str()));
             }
+            Rule::io_modifier_list => {
+                println!("{}", rule.as_str());
+                if rule.as_str() != "" {
+                    for io_modifier in rule.into_inner().next().unwrap().into_inner() {
+                        let mut filename: String = String::new();
+                        let io_modifier_clone = io_modifier.clone();
+                        for tok in io_modifier_clone.into_inner() {
+                            if tok.as_rule() == Rule::ident {
+                                filename = String::from(tok.as_str());
+                            }
+                        }
+
+                        match io_modifier.as_rule() {
+                            Rule::output_modifier => {
+                                command.outfile = filename;
+                            }
+                            Rule::input_modifier => {
+                                command.infile = filename;
+                            }
+                            Rule::output_append_modifier => {
+                                command.outfile = filename;
+                                command.append = true;
+                            }
+                            Rule::error_output_modifier => {
+                                command.errfile = filename;
+                            }
+                            Rule::error_append_modifier => {
+                                command.errfile = filename;
+                                command.append = true;
+                            }
+                            Rule::twogreat_modifier => {
+                                command.append = true;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            }
+            Rule::pipe_list => {
+                if rule.as_str() != "" {
+                    for pipe in rule.into_inner() {
+                        for p in pipe.into_inner() {
+                            if p.as_rule() == Rule::cmd_and_args {
+                                command.add_simple_command(String::from(p.as_str()));
+                            }
+                        }
+                    }
+                }
+            }
             _ => {}
         }
     }
