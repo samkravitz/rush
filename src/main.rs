170848948b823099bcf02052e372f5031cb77f77
Sam Kravitz <samkravitz99@gmail.com>
2022-06-06 09:53:18 -0400

    Allow multiple commands to be piped e.x. ls -al | grep Cargo

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod command;
 mod parse;
 
 use std::io::{stdin, stdout, Write};
+use std::process::{Command, Stdio};
 
 fn main() {
     loop {
@@ -12,15 +13,44 @@ fn main() {
         let mut line = String::new();
         stdin().read_line(&mut line).unwrap();
         let command = parse::parse(line.trim());
-        for simple_command in command.simple_commands {
-            let result = std::process::Command::new(&simple_command.cmd)
-                .args(&simple_command.args)
-                .spawn();
-            if let Ok(mut child) = result {
-                child.wait().ok();
+
+        let num_commands = command.simple_commands.len();
+        if num_commands == 0 {
+            continue;
+        }
+
+        let mut prev_command = None;
+        for i in 0..num_commands {
+            let simple_command = &command.simple_commands[i];
+            let is_final_command = i == num_commands - 1;
+
+            let stdin = prev_command.map_or(Stdio::inherit(), |output: std::process::Child| {
+                Stdio::from(output.stdout.unwrap())
+            });
+
+            let stdout = if is_final_command {
+                Stdio::inherit()
             } else {
-                println!("[rush]: Unknown command: {}", &simple_command.cmd);
-            }
+                Stdio::piped()
+            };
+
+            let child = Command::new(&simple_command.cmd)
+                .args(&simple_command.args)
+                .stdin(stdin)
+                .stdout(stdout)
+                .spawn();
+
+            prev_command = match child {
+                Ok(prev) => Some(prev),
+                Err(_) => {
+                    println!("[rush]: Unknown command: {}", &simple_command.cmd);
+                    None
+                }
+            };
+        }
+
+        if let Some(mut final_command) = prev_command {
+            final_command.wait().ok();
         }
     }
 }
